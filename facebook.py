6be7c7d885479fc4e7e97061af61c5b7e660a47c
mdaijaj <aijaj18@navgurukul.org>
2019-12-26 17:04:50 +0530

    update file

--- facebook.py
+++ facebook.py
@@ -13,8 +13,8 @@ option.add_argument("start-maximized")
 option.add_argument("--disable-extensions")      #"**--disable-notifications**"
 option.add_argument("--disable-notifications")
 
-username='8826616653'
-password = open("password", "r").read()
+username='enter your facebook id'
+password = "enter your password facebook password"
 
 driver = webdriver.Chrome("./chromedriver", chrome_options=option)
 url= 'https://www.facebook.com/'
@@ -70,69 +70,4 @@ input('Press anything to quit')
 # driver.close()
 driver.quit()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from selenium import webdriver 
-# import pprint, requests
-# from selenium.webdriver.support.ui import Select
-# from selenium.webdriver.common.keys import Keys
-# from time import sleep 
-# from getpass import getpass
-# from bs4 import BeautifulSoup
-# import time
-  
-# username='8826616653'                        
-# # password=input('Enter Password:')           #show password
-# password = open("password", "r").read()       #hide password
-
-# driver = webdriver.Chrome("./chromedriver") 
-# prefs = {"profile.default_content_setting_values.notifications" : 3} # to hide browser notifications
-# url= 'https://www.facebook.com/'
-# driver.get(url) 
-# print ("Opened facebook") 
-# sleep(1) 
-  
-# username_box = driver.find_element_by_id('email') 
-# username_box.send_keys(username) 
-# print ("Email Id entered") 
-# sleep(1) 
-  
-# password_box = driver.find_element_by_id('pass') 
-# password_box.send_keys(password) 
-# print ("Password entered") 
-# #   
-# login_box = driver.find_element_by_id('loginbutton') 
-# login_box.click() 
-
-
-# place="delhi"
-# market_url= url+ 'marketplace/'+ place 
-# # driver.get(market_url)
-
-# item_url= url + '/marketplace' + '/item/2381441782185456/'
-# data=driver.get(item_url)
-# print("plz wait")
-# sleep(5)
-# driver.execute_script('return document.documentElement.outerHTML')
-# # print(page)
-# # driver.implicitly_wait(5)
-# driver.find_element_by_tag_name("button").click()
-# # driver.find_element_by_link_text("Message")
-# driver.find_element_by_class_name("_2x3t _3dqo _645h")
-# # driver.find_element_by_xpath('')
-# # driver.find_element_by_xpath('//*[@id="js_s"]/div[2]/div[1]/div/div/div/div/div[2]/div/div[2]/div[2]/div/div/div/div/div/div[1]/div/span').click()
-# print("message sent successfully!")
-# input('Press anything to quit') 
-# driver.quit()
 
